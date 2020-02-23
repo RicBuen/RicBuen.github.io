d3af5c0421e2eedcecf3d6c04b5c1a3f621dbc79
RicBuen <rbjr55@outlook.com>
2020-02-23 17:16:57 -0500

    link update

--- js files/checkos.js
+++ js files/checkos.js
@@ -1,6 +1,6 @@
 function checkos()
 {
-   var downloadlink = "https://1drv.ms/u/s!Auzwb1HXIP44gadgqCxEfig0wdXmYQ?e=s4e3wu";
+   var downloadlink = "https://1drv.ms/u/s!Auzwb1HXIP44gadgqCxEfig0wdXmYQ?e=oDuYnY";
    var downloadBtn = document.getElementById("downloadBtn");
 
    if(navigator.appVersion.indexOf("Win") != -1)
