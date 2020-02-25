0f26d8858a9188489894ff614680919c2b56783d
RicBuen <rbjr55@outlook.com>
2020-02-24 20:55:12 -0500

    Changes to skillset.html (to be continued)

---
+++ js files/slideshow.js
@@ -0,0 +1,31 @@
+var sectionIndex = 1; //Starting starting index
+showSections(sectionIndex);
+
+//For changing sections
+function changeSections(n)
+{
+   showSections(sectionIndex += n); //sectionIndex = sectionIndex + n
+}
+
+//For showing the sections
+function showSections(n)
+{
+   var i;
+   var sections = document.getElementsByClassName("slideshow_sections"); //gets the section slides
+
+   if(n > sections.length) //if n is greater than the number of slides, set it back to 1
+   {
+      sectionIndex = 1;
+   }
+   if(n < -1)
+   {
+      sectionIndex = sections.length;
+   }
+
+   for (i = 0; i < sections.length; i++)
+   {
+      sections[i].style.display = "none";
+   }
+
+   sections[sectionIndex-1].style.display = "initial";
+}
