b2e825e8c7b9a99c63488b3ff3fbbf990556d097
RicBuen <rbjr55@outlook.com>
2020-02-24 21:19:31 -0500

    some changes

--- js files/slideshow.js
+++ js files/slideshow.js
@@ -13,14 +13,8 @@ function showSections(n)
    var i;
    var sections = document.getElementsByClassName("slideshow_sections"); //gets the section slides
 
-   if(n > sections.length) //if n is greater than the number of slides, set it back to 1
-   {
-      sectionIndex = 1;
-   }
-   if(n < -1)
-   {
-      sectionIndex = sections.length;
-   }
+   if(n > sections.length){ sectionIndex = 1 } //if n is greater than the number of slides, set it back to 1
+   if(n < 1){ sectionIndex = sections.length }
 
    for (i = 0; i < sections.length; i++)
    {
